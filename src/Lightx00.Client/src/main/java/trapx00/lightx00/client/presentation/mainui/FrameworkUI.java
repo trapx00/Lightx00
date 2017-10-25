df6d0de6ab503283a99246ccc009e5bded254148
Chen Junda <smallda@outlook.com>
2017-10-25 08:59:50 +0800

    refined ui

--- src/Lightx00.Client/src/main/java/trapx00/lightx00/client/presentation/mainui/FrameworkUI.java
+++ src/Lightx00.Client/src/main/java/trapx00/lightx00/client/presentation/mainui/FrameworkUI.java
@@ -1,9 +1,11 @@
 package trapx00.lightx00.client.presentation.mainui;
 
 import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.controls.JFXRippler;
 import javafx.fxml.FXML;
 import javafx.scene.control.SplitPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 public class FrameworkUI {
@@ -12,11 +14,17 @@ public class FrameworkUI {
 
     @FXML private Pane logPane;
     @FXML private Pane userPane;
-    @FXML private SplitPane bottomBasebox;
+    @FXML private Pane logBasePane;
+    @FXML private Pane userBasePane;
+    @FXML private GridPane bottomPane;
 
     public void initialize() {
         JFXRippler logPaneRipper = new JFXRippler(logPane);
+
         JFXRippler userPaneRipper = new JFXRippler(userPane);
+        logBasePane.getChildren().add(logPaneRipper);
+        userBasePane.getChildren().add(userPaneRipper);
+
     }
 
 }
