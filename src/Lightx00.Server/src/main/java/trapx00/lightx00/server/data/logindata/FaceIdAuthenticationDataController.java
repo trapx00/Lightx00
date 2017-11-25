ba8f7e335f5beb85aebf1695976870442f814c66
Chen Junda <smallda@outlook.com>
2017-11-25 14:26:41 +0800

    finished all docs

--- src/Lightx00.Server/src/main/java/trapx00/lightx00/server/data/logindata/FaceIdAuthenticationDataController.java
+++ src/Lightx00.Server/src/main/java/trapx00/lightx00/server/data/logindata/FaceIdAuthenticationDataController.java
@@ -1,22 +1,14 @@
 package trapx00.lightx00.server.data.logindata;
 
-import com.j256.ormlite.dao.Dao;
-import trapx00.lightx00.server.data.admindata.LoginService;
-import trapx00.lightx00.server.data.admindata.factory.LoginServiceFactory;
 import trapx00.lightx00.server.data.faceid.FaceIdAuthenticationService;
 import trapx00.lightx00.server.data.faceid.factory.FaceIdAuthenticationServiceFactory;
-import trapx00.lightx00.server.data.faceid.factory.FaceIdDaoFactory;
-import trapx00.lightx00.server.data.logindata.factory.LoginDataDaoFactory;
 import trapx00.lightx00.shared.dataservice.logindataservice.FaceIdAuthenticationDataService;
-import trapx00.lightx00.shared.po.employee.EmployeeFaceIdInfo;
-import trapx00.lightx00.shared.po.employee.EmployeePo;
 
 import java.rmi.RemoteException;
 import java.rmi.server.RMISocketFactory;
 import java.rmi.server.UnicastRemoteObject;
-import java.sql.SQLException;
 
-public class FaceIdDataController extends UnicastRemoteObject implements FaceIdAuthenticationDataService {
+public class FaceIdAuthenticationDataController extends UnicastRemoteObject implements FaceIdAuthenticationDataService {
     /**
      * Creates and exports a new UnicastRemoteObject object using an
      * anonymous port.
@@ -27,7 +19,7 @@ public class FaceIdDataController extends UnicastRemoteObject implements FaceIdA
      * @throws RemoteException if failed to export object
      * @since JDK1.1
      */
-    public FaceIdDataController() throws RemoteException {
+    public FaceIdAuthenticationDataController() throws RemoteException {
     }
 
     private FaceIdAuthenticationService service = FaceIdAuthenticationServiceFactory.getService();
--- src/Lightx00.Server/src/main/java/trapx00/lightx00/server/data/logindata/factory/FaceIdDataFactory.java
+++ src/Lightx00.Server/src/main/java/trapx00/lightx00/server/data/logindata/factory/FaceIdDataFactory.java
@@ -1,6 +1,6 @@
 package trapx00.lightx00.server.data.logindata.factory;
 
-import trapx00.lightx00.server.data.logindata.FaceIdDataController;
+import trapx00.lightx00.server.data.logindata.FaceIdAuthenticationDataController;
 import trapx00.lightx00.shared.dataservice.logindataservice.FaceIdAuthenticationDataService;
 
 import java.rmi.RemoteException;
@@ -10,7 +10,7 @@ public class FaceIdDataFactory {
 
     static {
         try {
-            dataService = new FaceIdDataController();
+            dataService = new FaceIdAuthenticationDataController();
         } catch (RemoteException e) {
             e.printStackTrace();
         }
