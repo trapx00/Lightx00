0e4aa9761fc2e199a6b222a308475f988189bf7e
Chen Junda <smallda@outlook.com>
2017-12-20 22:10:16 +0800

    changed init sequence of current employee in ui

--- src/Lightx00.Client/src/main/java/trapx00/lightx00/client/presentation/helpui/FrameworkUiManager.java
+++ src/Lightx00.Client/src/main/java/trapx00/lightx00/client/presentation/helpui/FrameworkUiManager.java
@@ -2,11 +2,13 @@ package trapx00.lightx00.client.presentation.helpui;
 
 import com.jfoenix.controls.JFXDialog;
 import javafx.scene.layout.StackPane;
+import trapx00.lightx00.client.presentation.adminui.EmployeeSelection;
 import trapx00.lightx00.client.presentation.mainui.FrameworkUiController;
 import trapx00.lightx00.client.vo.EmployeeVo;
 
 public class FrameworkUiManager {
     private static FrameworkUiController frameworkUiController;
+    private static EmployeeVo currentEmployee;
 
     public static FrameworkUiController getFrameworkUiController() {
         return frameworkUiController;
@@ -24,12 +26,16 @@ public class FrameworkUiManager {
         return promptDialogHelper.create(frameworkUiController.dialogContainer);
     }
 
+    public static void setCurrentEmployee(EmployeeVo employee) {
+        currentEmployee = employee;
+    }
+
     public static StackPane getDialogContainer() {
         return frameworkUiController.dialogContainer;
     }
 
     public static EmployeeVo getCurrentEmployee() {
-        return frameworkUiController.getEmployeeVo();
+        return currentEmployee;
     }
 
     public static void switchBackToHome() {
