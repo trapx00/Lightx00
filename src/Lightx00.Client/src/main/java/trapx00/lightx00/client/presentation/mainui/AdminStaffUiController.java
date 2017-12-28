58dbb10fc18a060a7e76c45096dc642f79c76745
HUGH <445073309@qq.com>
2017-12-28 09:33:42 +0800

    add backup button

--- src/Lightx00.Client/src/main/java/trapx00/lightx00/client/presentation/mainui/AdminStaffUiController.java
+++ src/Lightx00.Client/src/main/java/trapx00/lightx00/client/presentation/mainui/AdminStaffUiController.java
@@ -1,10 +1,16 @@
 package trapx00.lightx00.client.presentation.mainui;
 
 import javafx.event.ActionEvent;
+import trapx00.lightx00.client.blservice.logblservice.LogBackupBlService;
+import trapx00.lightx00.client.blservice.logblservice.LogBackupBlServiceFactory;
 import trapx00.lightx00.client.presentation.adminui.FaceIdRegistrationUiController;
 import trapx00.lightx00.client.presentation.adminui.UserManagementUiController;
+import trapx00.lightx00.client.presentation.helpui.FrameworkUiManager;
+import trapx00.lightx00.client.presentation.helpui.PromptDialogHelper;
+import trapx00.lightx00.shared.po.ResultMessage;
 
 public class AdminStaffUiController extends FrameworkUiController {
+    private LogBackupBlService logBackupBlService= LogBackupBlServiceFactory.getInstance();
 
     public void initialize() {
         // 必须有。调用基类初始化来初始化基类的元素。
@@ -20,6 +26,14 @@ public class AdminStaffUiController extends FrameworkUiController {
         switchFunction(FaceIdRegistrationUiController.class, "注册FaceId",true);
     }
     public void onLogBackUpFunctionClicked(ActionEvent actionEvent) {
-       // switchFunction(UserManagementUiController.class, "云备份日志",true);
+        if(logBackupBlService.backupLog()== ResultMessage.Success){
+            PromptDialogHelper.start("备份日志成功", "本地日志已经成功存储在云上。")
+                    .addCloseButton("好的", "CHECK", null)
+                    .createAndShow();
+        }else{
+            PromptDialogHelper.start("备份日志失败", "请稍后重试。")
+                    .addCloseButton("好的", "CHECK", null)
+                    .createAndShow();
+        }
     }
 }
